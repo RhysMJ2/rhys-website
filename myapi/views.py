42d9dc121474e14e77398b898b6a5b2fd908b0ea
Rhys Jones <rhysmjones11@gmail.com>
2020-04-19 16:29:16 +0100

    adjust rest framework

--- myapi/views.py
+++ myapi/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from rest_framework.views import APIView
 
 from .serializers import HeroSerializer
 from .models import Hero
@@ -7,3 +10,13 @@ from .models import Hero
 class HeroViewSet(viewsets.ModelViewSet):
     queryset = Hero.objects.all().order_by('name')
     serializer_class = HeroSerializer
+
+
+class RequestCount(APIView):
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
+
+    def get(self, request, format=None):
+        content = {
+            'status': 'request was permitted'
+        }
+        return Response(content)
