408afa23225faed5e1693bdd1c09bddacf50bd8a
Rhys Jones <rhysmjones11@gmail.com>
2020-04-23 19:57:29 +0100

    api subdomain created

--- firstdjango/hosts.py
+++ firstdjango/hosts.py
@@ -4,6 +4,6 @@ from django_hosts import patterns, host
 host_patterns = patterns(
     '',
     host(r'', settings.ROOT_URLCONF, name='www'),
-    host(r'account', 'firstdjango.accounts_urls', name='account')
-    # host(r'api', 'subdomains_tutorial.api_urls', name='api'),
+    host(r'account', 'firstdjango.accounts_urls', name='account'),
+    host(r'api', 'myapi.urls', name='api'),
 )
