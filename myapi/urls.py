a0a4b28ab9258d5277e7f2c341081615c5004f9d
Rhys Jones <rhysmjones11@gmail.com>
2020-04-23 20:47:21 +0100

    changed login for api

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from . import views
@@ -9,6 +10,7 @@ router.register(r'heroes', views.HeroViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', views.TestAPI.as_view()),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('test/', include((router.urls, 'api'), namespace='api'))
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include(('firstdjango.accounts_urls', 'account'), namespace='rest_framework')),
+    path('test/', include((router.urls, 'api'), namespace='api')),
 ]
