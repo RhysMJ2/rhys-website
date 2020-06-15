e9cfea18e5c9e8826f99329281bd0142a2d9b11c
Rhys Jones <rhysmjones11@gmail.com>
2020-06-15 15:54:16 +0100

    removed fields signup form
    removed the first and last name field from the signup form, these can be changed when account is created in the settings of the profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,8 +4,12 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
+    email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput(),
+                            help_text="Required. You will have to verify this email before the account is activated")
+
+    username = forms.CharField(max_length=15, required=True,
+                               help_text="Required. 15 characters or less")
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
