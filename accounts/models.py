7d06717008e223b4d6aa1aef7aaf4f2829323297
Rhys Jones <rhysmjones11@gmail.com>
2020-06-18 16:25:16 +0100

    profile database table

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name="profiles", on_delete=models.CASCADE)
+    bio = models.CharField(max_length=350)
+
+    def __str__(self):
+        return self.user.username
--- boards/models.py
+++ boards/models.py
@@ -27,6 +27,7 @@ class Board(models.Model):
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
+    # todo change on_delete https://docs.djangoproject.com/en/3.0/ref/models/fields/
     board = models.ForeignKey(Board, related_name='topics', on_delete=models.DO_NOTHING)
     starter = models.ForeignKey(User, related_name='topics', on_delete=models.DO_NOTHING)
     views = models.PositiveIntegerField(default=0)
