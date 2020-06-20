2e190a03700f4581fa6d9b7559b24276c2c612ef
Rhys Jones <rhysmjones11@gmail.com>
2020-06-20 18:55:34 +0100

    posts in profile

--- accounts/views.py
+++ accounts/views.py
@@ -161,7 +161,6 @@ class UserProfileView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context["profile_data"] = User.objects.get(username=self.user.username)
-
         return super().get_context_data(**context)
 
     def get_queryset(self):
