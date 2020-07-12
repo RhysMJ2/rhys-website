13642dfac50f5c4742cabc252644885d588fa37f
Rhys Jones <rhysmjones11@gmail.com>
2020-07-12 19:24:55 +0100

    POST request permission

--- myapi/permissions.py
+++ myapi/permissions.py
@@ -5,8 +5,15 @@ from rest_framework import permissions
 class EditBoardOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        hasPermission = False
         if request.method in permissions.SAFE_METHODS:
-            return True
+            hasPermission = True
 
         # todo return true for post if has permission
-        return (request.user.has_perm("boards.can_edit_board") and request.method == "PUT")
+        elif request.method == "PUT":
+            hasPermission = request.user.has_perm("boards.can_edit_board")
+
+        elif request.method == "POST":
+            hasPermission = request.user.has_perm("boards.can_add_board")
+
+        return hasPermission
